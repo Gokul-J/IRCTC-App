1886bfa8bf17bc7c913d2707d1e1f323c7c73b4d
Gokul-J <jgokul2000@gmail.com>
2020-08-20 23:08:30 +0530

    Deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   // mongoURI : "mongodb://localhost/irctc-app",
-  // mongoURI : "mongodb+srv://Gokul-J:jothi@2009@irctc-app.q9gzc.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  mongoURI : "mongodb+srv://Gokul-J:jothi@2009@irctc-app.q9gzc.mongodb.net/<dbname>?retryWrites=true&w=majority",
   secretOrKey: "I Love Coding"
 }
